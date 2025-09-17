2b0ac40ba96f3ae0207e1b5e10e47002d889c76b
khumora77 <xumora427@gmail.com>
2025-09-17 17:56:33 +0500

    navbar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,13 +1,20 @@
-import { SidebarProvider } from '@/components/ui/sidebar'
-import React from 'react'
-import AppSidebar from './_components/sidebar/app-sidebar'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import React from "react";
+import AppSidebar from "./_components/sidebar/app-sidebar";
+import AppNavbar from "./_components/navbar/app-navbar";
+import { RootLayoutProps } from "@/types";
 
-const BrowseLayout = () => {
+const BrowseLayout = ({ children }: RootLayoutProps) => {
   return (
     <SidebarProvider>
-        <AppSidebar/>
-    </SidebarProvider>
-  )
-}
+      <AppSidebar />
 
-export default BrowseLayout
+      <main className="w-full h-full">
+        <AppNavbar />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default BrowseLayout;
